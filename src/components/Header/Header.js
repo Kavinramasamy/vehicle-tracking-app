af113824d0dbe854299b81298051ac180d6ccc7c
Kavinramasamy <124972388+Kavinramasamy@users.noreply.github.com>
2024-07-24 08:06:12 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function Header({title}) {
+
+   
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+            <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Vehicle Live Tracking System
+            </Typography>
+            
+               
+            </Toolbar>
+        </AppBar>
+      </Box>
+    )
+}
+
+export default Header
---
+++ src/components/gMap/config.js
@@ -0,0 +1,7 @@
+const config = {
+    mapsKey: 'AIzaSyCwAnctBuWMAO7--R1g1BYsmdi_yc7mJQs',
+    myMapyKey: 'AIzaSyCwAnctBuWMAO7--R1g1BYsmdi_yc7mJQs',
+}
+
+export default config;
+
